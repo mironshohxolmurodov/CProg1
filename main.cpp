30fcf759b8b5115ced766287ade3e2a3d045110b
250306 <250306@newuu.uz>
2025-09-11 10:57:19 +0500

    LABWORK

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+//problem1
+// int main() {
+//     int intNumber=24;
+//     float floatNumber=3.14159;
+//     double doubleNumber=45.1234;
+//     bool boolean=true;
+//     char charName='A';
+//     cout<<"intNumber: "<<intNumber<<", Size: "<<sizeof(intNumber)<<endl;
+//     cout<<"floatNumber: "<<floatNumber<<", Size: "<<sizeof(floatNumber)<<endl;
+//     cout<<"doubleNumber: "<<doubleNumber<<", Size: "<<sizeof(doubleNumber)<<endl;
+//     cout<<"boolean: "<<boolean<<", Size: "<<sizeof(boolean)<<endl;
+//     cout<<"charName: "<<charName<<", Size: "<<sizeof(charName)<<endl;
+//     return 0;
+// }
+
+//problem2
+// int main() {
+//     string name="Mironshoh Xolmurodov";
+//     int id=233242;
+//     string favsong="Demons - Imagine Dragons";
+//     string favmovie="Forest Gump";
+//     cout<<name<<endl;
+//     cout<<id<<endl;
+//     cout<<favsong<<endl;
+//     cout<<favmovie<<endl;
+//     return 0;
+// }
+
+//problem3
+// int main() {
+//     cout<<"\"I’m standing\" on the edge of some crazy cliff. \nWhat I have to do, "
+//                  "I have to catch everybody\\ \nif they start to go over the cliff\u2014 I mean\\ "
+//                  "\n\\if they’re running and they don’t look \nwhere they’re going I have to come "
+//                  "out \nfrom somewhere and    catch them."<<endl;
+// }
+
+//problem4
+// int main() {
+//     int a=13;
+//     char b='R';
+//     float c=float(a)/3;
+//     int d=int(b);
+//     float e=float(a)+d/5;
+//     char f=b+(a/10-1);
+//     cout<<c<<" "<<d<<" "<<e<<" "<<f<<endl;
+//     return 0;
+// }
+
+//problem5
+// int main() {
+//     int val1=12;
+//     int val2=38;
+//     cout<<"Sum="<<val1+val2<<endl;
+//     cout<<"Product="<<val1*val2<<endl;
+//     return 0;
+// }
+
+//problem6
+// int main() {
+//     int val1;
+//     int val2;
+//     cin >> val1 >> val2;
+//     cout << val1 << "+" << val2 <<"=" << val1 + val2 << endl;
+//     return 0;
+// }
+
+//problem7
+// int main() {
+//     float x;
+//     float y;
+//     cin>>x>>y;
+//     cout<<"t="<<(3*(x*x+3))/(y/4+3)<<y<<endl;
+//     return 0;
+// }
+
+//problem8
+// int main() {
+//     float d;
+//     cin>>d;
+//     float r=d/2;
+//     cout<<4*r*r*3.14159<<endl;
+//     return 0;
+// }
+
+//problem9
+// int main() {
+//     float v0;
+//     float v1;
+//     float t;
+//     cin >> v0 >> v1 >> t;
+//     cout<<(v1-v0)/t<<endl;
+//     return 0;
+// }
