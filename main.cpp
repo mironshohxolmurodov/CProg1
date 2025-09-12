0f87a5068b6dbb4acbdc5797a5dabcaa219827aa
250306 <250306@newuu.uz>
2025-09-12 12:29:01 +0500

    LabWork11To22

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <valarray>
+#include <math.h>
+using namespace std;
+
+//problem11
+// int main() {
+//     double x = 1.13e3;
+//     double y = 411e-4;
+//     cout << x << " " << y << endl;
+//     return 0;
+// }
+
+//problem12
+// int main() {
+//     int N=241;
+//     cout<<"In first group: "<<N/11<<endl;
+//     cout<<"In last group: "<<N%11<<endl;
+//     return 0;
+// }
+
+//problem13
+// int main() {
+//     float side;
+//     cout << "Enter one side of Hexagon: " << endl;
+//     cin>>side;
+//     cout << "The are a of hexagon is " << 3*sqrt(3)*side*side/2 << endl;
+//     return 0;
+// }
+
+//problem14
+// int main() {
+//     int num=31;
+//     num=((((++num)*3-14)/4)-1)%9;
+//     cout << num << endl;
+//     return 0;
+// }
+
+//problem15
+// int main() {
+//     char m='A';
+//     cout<<char(m+7)<<char(m+4)<<char(m+11)<<char(m+11)<<char(m+14)<<char(m-32)<<endl;
+//     return 0;
+// }
+
+//problem16
+// int main() {
+//     float x=17;
+//     float y=1;
+//     float z=(x+(++x))/((y+y+y)*(y+y));
+//     cout<<z<<endl;
+//     return 0;
+// }
+
+//problem17
+// int main() {
+//     int num1;
+//     int num2;
+//     cin >> num1 >> num2;
+//     cout << (num1 == num2) << endl;
+//     return 0;
+// }
+
+//problem18
+// int main() {
+//     int a;
+//     int b;
+//     cin >> a >> b;
+//     cout << ((a<140)&&(a<b))<< endl;
+//     return 0;
+// }
+
+//problem19
+// int main() {
+//     int a;
+//     int b;
+//     int c;
+//     cin >> a >> b >> c;
+//     cout << ((a>c)&&(a>b)) << endl;
+//     return 0;
+// }
+
+//problem20
+// int main() {
+//     int n1;
+//     int n2;
+//     int n3;
+//     int n4;
+//     int n5;
+//     cin >> n1 >> n2 >> n3 >> n4 >> n5;
+//     cout << ((n1>=0)||(n2>=0)||(n3>=0)||(n4>=0)||(n5>=0)) << endl;
+//     return 0;
+// }
+
+//problem21
+// int main() {
+//     int n1;
+//     int n2;
+//     cin >> n1 >> n2;
+//     cout << (((n1+n2)>30)&&((n1*n2)>30)) <<" "<< (((n1+(n2-10))>30)&&((n1*(n2-10))>30)) <<" "<< ((n1>30)||(n2>30)) << endl;
+//     return 0;
+// }
+
+//problem22
+// int main() {
+//     float s;
+//     float m;
+//     cin >> s>>m;
+//     float m1=m*30.48;
+//     cout<<(m1>s)<<endl;
+//     return 0;
+// }
